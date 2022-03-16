8954f2798be5f5199a3c66fd10565edfe95b1e96
LighttSof <egorikse@mail.ru>
2022-03-16 23:03:21 +0300

    initial

---
+++ config.py
@@ -0,0 +1,4 @@
+from tokenize import Token
+
+
+Token = "26f3859bf44544cd8ac90d908aa0451b"
---
+++ main.py
@@ -0,0 +1,5 @@
+import requests
+respons = requests.get('https://api.spotify.com/v1/albums/id/');
+print(response.status_code)
+
+#TODO ПОИСК ПЛЕЛИСТОВ ПО ПРОФИЛЮ
